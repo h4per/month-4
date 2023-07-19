b20e20576050a0527991e4f618ae082f61362397
h4per <h4peryt@gmail.com>
2023-07-19 16:58:26 +0600

    dz2 update

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
-
+request = HttpRequest()
 def index_view(request):
     return HttpResponse ("Главная страница")
 

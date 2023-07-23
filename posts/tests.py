4defd03ba35e23d75b8149bab207847dffe3d982
h4per <h4peryt@gmail.com>
2023-07-23 19:24:45 +0600

    dz3 test

--- posts/tests.py
+++ posts/tests.py
@@ -6,27 +6,25 @@ class PostsTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
+
     def test_index_bek(self):
         response = self.client.get(reverse("index-bek"))
-        exp_data = "Главная страница"
 
-        self.assertEqual(response.content.decode(), exp_data)
+        self.assertTemplateUsed(response, 'posts/index.html')
         self.assertEqual(response.status_code, 200)
 
 
     def test_get_contacts(self):
         response = self.client.get(reverse('contacts'))
-        exp_data = "Контакты"
 
-        self.assertEqual(response.content.decode(), exp_data)
-        self.assertEqual(response.status_code, 500)
+        self.assertTemplateUsed(response, 'posts/contacts.html')
+        self.assertEqual(response.status_code, 200)
 
 
     def test_get_about(self):
         response = self.client.get(reverse('about'))
-        exp_data = "О нас"
 
-        self.assertEqual(response.content.decode(), exp_data)
-        self.assertEqual(response.status_code, 400)
+        self.assertTemplateUsed(response, 'posts/abourt.html')
+        self.assertEqual(response.status_code, 200)
 
     
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,30 @@
-# from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest 
 
-# request = HttpRequest()
-def index_bek(requests):
-    return HttpResponse ("Главная страница", status=200)
+
+request = HttpRequest()
+def index_bek(request):
+    context = {
+        "name": "Oleg",
+        "title": "Главная страница",
+        "my_list": [1,3,4,2]
+    }
+    return render(request, "posts/index.html", context=context)
+
 
 
 def get_contacts(request):
-    return HttpResponse('Контакты', status=200)
+    context = {
+        "title": "Контакты",
+    }
+    return render(request, "posts/contacts.html", context)
+
 
 
 def get_about(request):
-    return HttpResponse('О нас',status=200)
+    context = {
+        "title": "О нас",
+        "content": "Анекдот: колобок моется,моется... Вышел из душа и говорит: 'Блиин я голову забыл помыть'"
+    }
+    return render(request, "posts/about.html", context)
 
